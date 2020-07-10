aff571771ae27377cf29f6c8b71ad41ec467c7e2
Uro3 <miyakawa.uro3@gmail.com>
2020-07-11 05:40:37 +0900

    remove redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :redis_store, {
-  servers: [
-    { host: ENV['REDIS_URL'], port: 6379, db: 0, namespace: "session" }
-  ],
-  key: '_rails-k8s-practice_session'
-}
+Rails.application.config.session_store :cookie_store, key: '_rails-k8s-practice_session'
+# Rails.application.config.session_store :redis_store, {
+#   servers: [
+#     { host: ENV['REDIS_URL'], port: 6379, db: 0, namespace: "session" }
+#   ],
+#   key: '_rails-k8s-practice_session'
+# }
