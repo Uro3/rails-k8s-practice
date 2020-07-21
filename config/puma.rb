8a88cd678492c88cd7dfe7c877579fa0dff42be0
Uro3 <kazuya.miyakawa@icare.jpn.com>
2020-07-21 15:07:20 +0900

    output puma logs to stdout

--- config/puma.rb
+++ config/puma.rb
@@ -6,5 +6,3 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-
-stdout_redirect "#{Rails.root}/log/puma.stdout.log", "#{Rails.root}/log/puma.stderr.log", true
